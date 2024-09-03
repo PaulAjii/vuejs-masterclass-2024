8fa8b962523cfac73f3f73c873597f18ac91df91
PaulAjii <paulajii134@gmail.com>
2024-09-03 03:10:08 +0100

    Routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
+import HomeView from '@/views/HomeView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+
+        {
+            path: '/projects',
+            name: 'projects',
+            component: () => import('@/views/ProjectView.vue')
+        },
+
+        {
+            path: '/projects/:id',
+            name: 'single-project',
+            component: () => import('@/views/SingleProject.vue')
+        }
+    ]
 });
 
 export default router;
